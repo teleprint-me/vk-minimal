0bf60ea86d08be94d72b64b5075081f33e20c8eb
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-05-17 19:15:22 -0400

    vk(chore): Silence is golden (unless otherwise specified)

--- src/vk/alloc.c
+++ src/vk/alloc.c
@@ -6,12 +6,8 @@
 #include "core/logger.h"
 #include "vk/alloc.h"
 
-void* VKAPI_CALL vk_lease_alloc(
-    void* pUserData,
-    size_t size,
-    size_t alignment,
-    VkSystemAllocationScope scope
-) {
+void* VKAPI_CALL
+vk_lease_alloc(void* pUserData, size_t size, size_t alignment, VkSystemAllocationScope scope) {
     LeaseOwner* owner = (LeaseOwner*) pUserData;
     if (!owner) {
         LOG_ERROR("[VK_ALLOC] Owner reference failure: address=%p", pUserData);
@@ -23,21 +19,23 @@ void* VKAPI_CALL vk_lease_alloc(
         LOG_ERROR(
             "VK_ALLOC] Failed to alloc: "
             "address=%p, size=%zu, align=%zu, scope=%d",
-            owner, size, alignment, scope
+            owner,
+            size,
+            alignment,
+            scope
         );
         return NULL;
     }
 
-    LOG_INFO("[VK_ALLOC] %p (%zu bytes, %zu aligned)", address, size, alignment);
+#ifdef ENABLE_DEBUG
+    LOG_DEBUG("[VK_ALLOC] %p (%zu bytes, %zu aligned)", address, size, alignment);
+#endif
+
     return address;
 }
 
 void* VKAPI_CALL vk_lease_realloc(
-    void* pUserData,
-    void* pOriginal,
-    size_t size,
-    size_t alignment,
-    VkSystemAllocationScope scope
+    void* pUserData, void* pOriginal, size_t size, size_t alignment, VkSystemAllocationScope scope
 ) {
     (void) scope;
     LeaseOwner* owner = (LeaseOwner*) pUserData;
@@ -52,17 +50,24 @@ void* VKAPI_CALL vk_lease_realloc(
     }
 
     LeaseTenant* tenant = lease_get_tenant(owner, pOriginal);
-    if (tenant && tenant->object && tenant->object->address) { 
-        LOG_INFO("[VK_REALLOC] %p → %p (%zu bytes)", pOriginal, tenant->object->address, size);
+    if (tenant && tenant->object && tenant->object->address) {
+#ifdef ENABLE_DEBUG
+        LOG_DEBUG("[VK_REALLOC] %p → %p (%zu bytes)", pOriginal, tenant->object->address, size);
+#endif
         return tenant->object->address;
     }
 
-    LOG_ERROR("VK_REALLOC] Failed to find tenant: address=%p, size=%zu", pOriginal, size);
+#ifdef ENABLE_DEBUG
+    LOG_DEBUG("VK_REALLOC] Failed to find tenant: address=%p, size=%zu", pOriginal, size);
+#endif
+
     return NULL;
 }
 
 void VKAPI_CALL vk_lease_free(void* pUserData, void* pMemory) {
-    if (!pUserData || !pMemory) { return; }
+    if (!pUserData || !pMemory) {
+        return;
+    }
 
     LeaseOwner* owner = (LeaseOwner*) pUserData;
     if (!owner) {
@@ -76,35 +81,50 @@ void VKAPI_CALL vk_lease_free(void* pUserData, void* pMemory) {
         return;
     }
 
+#ifdef ENABLE_DEBUG
+    LOG_DEBUG("[VK_FREE] %p", pMemory);
+#endif
+
     lease_terminate(owner, pMemory);
-    LOG_INFO("[VK_FREE] %p", pMemory);
 }
 
 void VKAPI_CALL vk_lease_internal_alloc(
-    void* pUserData,
-    size_t size,
-    VkInternalAllocationType type,
-    VkSystemAllocationScope scope
+    void* pUserData, size_t size, VkInternalAllocationType type, VkSystemAllocationScope scope
 ) {
-    LOG_INFO("[VK_INTERNAL_ALLOC] owner=%p, size=%zu, type=%d, scope=%d", pUserData, size, type, scope);
+#ifndef ENABLE_DEBUG
+(void) pUserData;
+(void) size;
+(void) type;
+(void) scope;
+#endif
+#ifdef ENABLE_DEBUG
+    LOG_DEBUG(
+        "[VK_INTERNAL_ALLOC] owner=%p, size=%zu, type=%d, scope=%d", pUserData, size, type, scope
+    );
+#endif
 }
 
 void VKAPI_CALL vk_lease_internal_free(
-    void* pUserData,
-    size_t size,
-    VkInternalAllocationType type,
-    VkSystemAllocationScope scope
+    void* pUserData, size_t size, VkInternalAllocationType type, VkSystemAllocationScope scope
 ) {
-    LOG_INFO("[VK_INTERNAL_FREE] owner=%p, size=%zu, type=%d, scope=%d", pUserData, size, type, scope);
+#ifndef ENABLE_DEBUG
+(void) pUserData;
+(void) size;
+(void) type;
+(void) scope;
+#endif
+#ifdef ENABLE_DEBUG
+    LOG_DEBUG(
+        "[VK_INTERNAL_FREE] owner=%p, size=%zu, type=%d, scope=%d", pUserData, size, type, scope
+    );
+#endif
 }
 
 VkAllocationCallbacks VKAPI_CALL vk_lease_callbacks(LeaseOwner* owner) {
-    return (VkAllocationCallbacks) {
-        .pUserData = owner,  // or pass this dynamically
-        .pfnAllocation = vk_lease_alloc,
-        .pfnReallocation = vk_lease_realloc,
-        .pfnFree = vk_lease_free,
-        .pfnInternalAllocation = vk_lease_internal_alloc,
-        .pfnInternalFree = vk_lease_internal_free
-    };    
+    return (VkAllocationCallbacks) {.pUserData = owner, // or pass this dynamically
+                                    .pfnAllocation = vk_lease_alloc,
+                                    .pfnReallocation = vk_lease_realloc,
+                                    .pfnFree = vk_lease_free,
+                                    .pfnInternalAllocation = vk_lease_internal_alloc,
+                                    .pfnInternalFree = vk_lease_internal_free};
 }
