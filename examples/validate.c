8e0b9cc51a4e86fff9ab903bdb5e504839e2f13b
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-05-25 04:51:54 -0400

    validate(refactor): Add a layer validation factory

--- examples/validate.c
+++ examples/validate.c
@@ -14,108 +14,155 @@
 
 #define VALIDATION_LAYER_COUNT 1 /// @warning Cannot be a variable
 
-typedef struct Layer {
+typedef struct LayerRequest {
     uint32_t count;
     const char* const* names;
+} LayerRequest;
+
+typedef struct LayerAvailable {
+    uint32_t count;
+    VkLayerProperties* properties;
+} LayerAvailable;
+
+typedef struct Layer {
+    LayerRequest* request;
+    LayerAvailable* available;
 } Layer;
 
-typedef struct LayerProperty {
-    uint32_t count;
-    VkLayerProperties* layers;
-} LayerProperty;
+LayerRequest*
+layer_create_request(LeaseOwner* owner, const char* const* names, const uint32_t count) {
+    LayerRequest* request
+        = lease_alloc_owned_address(owner, sizeof(LayerRequest), alignof(LayerRequest));
+    if (!request) {
+        return NULL;
+    }
+
+    *request = (LayerRequest) {
+        .count = count,
+        .names = names,
+    };
+
+    return request;
+}
+
+LayerAvailable* layer_create_available(LeaseOwner* owner, LayerRequest* request) {
+    if (!request || !request->names || !(*request->names) || 0 == request->count) {
+        LOG_ERROR("Invalid arguments (layers=%p, layer_count=%u)", request->names, request->count);
+        return NULL;
+    }
+
+    LayerAvailable* available
+        = lease_alloc_owned_address(owner, sizeof(LayerAvailable), alignof(LayerAvailable));
+    if (!available) {
+        return NULL;
+    }
+
+    VkResult result;
+    result = vkEnumerateInstanceLayerProperties(&available->count, NULL);
+    if (VK_SUCCESS != result) {
+        LOG_ERROR("Failed to enumerate layer property count (error code: %u)", result);
+        return NULL;
+    }
+    if (0 == available->count) {
+        LOG_ERROR("No validation layers available");
+        return NULL;
+    }
+
+    size_t size = available->count * sizeof(VkLayerProperties);
+    available->properties = lease_alloc_owned_address(owner, size, alignof(VkLayerProperties));
+    if (!available->properties || !memset(available->properties, 0, size)) {
+        LOG_ERROR("Memory allocation failed for layer properties");
+        return NULL;
+    }
+
+    result = vkEnumerateInstanceLayerProperties(&available->count, available->properties);
+    if (VK_SUCCESS != result) {
+        LOG_ERROR("Failed to enumerate layer properties (error code: %u)", result);
+        return NULL;
+    }
+
+    return available;
+}
 
 Layer* layer_create(LeaseOwner* owner, const char* const* names, const uint32_t count) {
+    if (!owner || !names || !(*names) || 0 == count) {
+        LOG_ERROR("Invalid arguments (owner=%p, layers=%p, layer_count=%u)", owner, names, count);
+        return NULL;
+    }
+
+    LayerRequest* request = layer_create_request(owner, names, count);
+    if (!request) {
+        return NULL;
+    }
+
+    LayerAvailable* available = layer_create_available(owner, request);
+    if (!available) {
+        return NULL;
+    }
+
     Layer* layer = lease_alloc_owned_address(owner, sizeof(Layer), alignof(Layer));
     if (!layer) {
         return NULL;
     }
 
     *layer = (Layer) {
-        .count = count,
-        .names = names,
+        .available = available,
+        .request = request,
     };
 
     return layer;
 }
 
-LayerProperty* layer_create_property(LeaseOwner* owner, Layer* layer) {
-    if (!layer || !layer->names || !(*layer->names) || 0 == layer->count) {
-        LOG_ERROR("Invalid arguments (layers=%p, layer_count=%u)", layer->names, layer->count);
-        return NULL;
-    }
-
-    LayerProperty* property
-        = lease_alloc_owned_address(owner, sizeof(LayerProperty), alignof(LayerProperty));
-    if (!property) {
-        return NULL;
-    }
-
-    VkResult result;
-    result = vkEnumerateInstanceLayerProperties(&property->count, NULL);
-    if (VK_SUCCESS != result) {
-        LOG_ERROR("Failed to enumerate layer property count (error code: %u)", result);
-        return NULL;
-    }
-    if (0 == property->count) {
-        LOG_ERROR("No validation layers available");
-        return NULL;
-    }
-
-    size_t size = property->count * sizeof(VkLayerProperties);
-    property->layers = lease_alloc_owned_address(owner, size, alignof(VkLayerProperties));
-    if (!property->layers || !memset(property->layers, 0, size)) {
-        LOG_ERROR("Memory allocation failed for layer properties");
-        return NULL;
-    }
-
-    result = vkEnumerateInstanceLayerProperties(&property->count, property->layers);
-    if (VK_SUCCESS != result) {
-        LOG_ERROR("Failed to enumerate layer properties (error code: %u)", result);
-        return NULL;
-    }
-
-    return property;
-}
-
-bool layer_validate(Layer* requested, LayerProperty* available) {
-    for (uint32_t i = 0; i < requested->count; ++i) {
+bool layer_validate(Layer* layer) {
+    for (uint32_t i = 0; i < layer->request->count; ++i) {
         bool found = false;
-        for (uint32_t j = 0; j < available->count; ++j) {
-            if (strcmp(requested->names[i], available->layers[j].layerName) == 0) {
+        for (uint32_t j = 0; j < layer->available->count; ++j) {
+            if (strcmp(layer->request->names[i], layer->available->properties[j].layerName)
+                == 0) {
                 found = true;
                 break;
             }
         }
         if (!found) {
-            LOG_WARN("Requested layer not available: %s", requested->names[i]);
+            LOG_WARN("Requested layer not available: %s", layer->request->names[i]);
             return false;
         }
     }
     return true;
 }
 
-void layer_log(LayerProperty* property) {
-    LOG_INFO("Supported Property Count: %zu", property->count);
-    for (uint32_t i = 0; i < property->count; i++) {
+VkLayerProperties* layer_query() {
+    return NULL; // stub
+}
+
+void layer_log(Layer* layer) {
+    LOG_INFO("[VkLayerProperties] request->count=%zu", layer->request->count);
+    for (uint32_t i = 0; i < layer->request->count; i++) {
+        LOG_INFO("[VkLayerProperties] request->names[%zu]=%s", i, layer->request->names[i]);
+    }
+    LOG_INFO("Property Count: %zu", layer->available->count);
+    for (uint32_t i = 0; i < layer->available->count; i++) {
         LOG_INFO(
-            "Supported Property: index=%zu, name=%s, description=%s",
+            "Property: index=%zu, name=%s, description=%s",
             i,
-            property->layers[i].layerName,
-            property->layers[i].description
+            layer->available->properties[i].layerName,
+            layer->available->properties[i].description
         );
     }
 }
 
 int main(void) {
-    const char* const validation_layers[VALIDATION_LAYER_COUNT] = {"VK_LAYER_KHRONOS_validation"};
+    const char* const layers[VALIDATION_LAYER_COUNT] = {"VK_LAYER_KHRONOS_validation"};
     LeaseOwner* owner = lease_create_owner(1024); // size in bytes
-    Layer* layer = layer_create(owner, validation_layers, VALIDATION_LAYER_COUNT);
-    LayerProperty* property = layer_create_property(owner, layer);
-    layer_log(property);
-    if (!layer_validate(layer, property)) {
+    Layer* layer = layer_create(owner, layers, VALIDATION_LAYER_COUNT);
+    if (!layer) {
+        return EXIT_FAILURE;
+    }
+    layer_log(layer);
+    if (!layer_validate(layer)) {
         LOG_ERROR("One or more requested layers are not available.");
     }
-    LOG_INFO("Selected layer: %s", validation_layers[0]);
+    LOG_INFO("Selected layer: %s", layers[0]);
     lease_free_owner(owner); // free everything
-    return 0;
+    return EXIT_SUCCESS;
 }
