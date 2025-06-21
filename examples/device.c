67b82bbefece114adf58fd2bf3af9bff944c9fea
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-06-21 01:30:05 -0400

    device(refactor): Encapsulate the context

--- examples/device.c
+++ examples/device.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 
 typedef struct VkcPhysicalDevice {
+    HashMap* ctx;
     VkPhysicalDevice* list;
     VkPhysicalDevice selected;
     VkPhysicalDeviceProperties properties;
@@ -26,17 +27,15 @@ int main(void) {
         return EXIT_FAILURE;
     }
 
-    // Create the allocator context
-    HashMap* ctx = hash_map_create(1024, HASH_MAP_KEY_TYPE_ADDRESS);
-    if (!ctx) {
-        LOG_ERROR("Failed to create device context.");
-        goto cleanup_instance;
-    }
-
     VkcPhysicalDevice device = {0};
     device.queue_family_index = UINT32_MAX;
     device.list = NULL;
     device.selected = VK_NULL_HANDLE;
+    device.ctx = hash_map_create(1024, HASH_MAP_KEY_TYPE_ADDRESS);
+    if (!device.ctx) {
+        LOG_ERROR("Failed to create device context.");
+        goto cleanup_instance;
+    }
 
     VkResult result = vkEnumeratePhysicalDevices(instance->object, &device.count, NULL);
     if (result != VK_SUCCESS || device.count == 0) {
@@ -50,16 +49,28 @@ int main(void) {
     LOG_DEBUG("Vulkan-compatible devices found (VkResult: %d, Count: %u)", result, device.count);
 
     device.list = hash_page_malloc(
-        ctx, sizeof(VkPhysicalDevice) * device.count, alignof(VkPhysicalDevice)\
+        device.ctx, sizeof(VkPhysicalDevice) * device.count, alignof(VkPhysicalDevice)\
     );
     if (!device.list) {
         LOG_ERROR("Failed to allocate physical device list.");
         goto cleanup_context;
     }
 
+    result = vkEnumeratePhysicalDevices(instance->object, &device.count, device.list);
+    if (result != VK_SUCCESS) {
+        LOG_ERROR("Failed to set physical device list.");
+        goto cleanup_context;
+    }
+    for (uint32_t i = 0; i < device.count; i++) {
+        VkPhysicalDevice tmp = device.list[i];
+        VkPhysicalDeviceProperties props = {0};
+        vkGetPhysicalDeviceProperties(tmp, &props);
+        LOG_INFO("Device %u: %s", i, props.deviceName);
+    }
+
 cleanup_context:
-    hash_page_free_all(ctx);
-    hash_map_free(ctx);
+    hash_page_free_all(device.ctx);
+    hash_map_free(device.ctx);
 cleanup_instance:
     vkc_instance_destroy(instance);
     return EXIT_SUCCESS;
