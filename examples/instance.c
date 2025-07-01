a65f96fe4661f536d8b0b408a67b3b709f3df8d2
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-07-01 16:39:18 -0400

    instance(refactor): Start from scratch

--- examples/instance.c
+++ examples/instance.c
@@ -3,19 +3,281 @@
  */
 
 #include "core/logger.h"
+#include "utf8/raw.h"
+#include "vk/allocator.h"
 #include "vk/instance.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(void) {
-    VkcInstance* instance = vkc_instance_create(1024);
-    if (!instance) {
-        LOG_ERROR("Failed to create Vulkan instance!");
-        return EXIT_FAILURE;
+    /**
+     * @name Debug Environment
+     * @brief Enables verbose logging when VKC_DEBUG=1 is set.
+     * @{
+     */
+
+#if defined(VKC_DEBUG) && (1 == VKC_DEBUG)
+    LOG_DEBUG("[VkCompute] Debug mode.");
+#else
+    LOG_INFO("[VkCompute] Release mode.");
+#endif
+
+    /** @} */
+
+    /**
+     * @name Memory Allocators
+     * @{
+     */
+
+    PageAllocator* pager = page_allocator_create(1024);
+    VkAllocationCallbacks vkAllocationCallback = vkc_page_callbacks(pager);
+
+    /** @} */
+
+    /**
+     * @name Global Result
+     * @{
+     */
+
+    VkResult result; // Result codes and handles
+
+    /** @} */
+
+    /**
+     * @name Instance Layer Properties
+     * @{
+     */
+
+    uint32_t vkInstanceLayerPropertyCount = 0;
+    result = vkEnumerateInstanceLayerProperties(&vkInstanceLayerPropertyCount, NULL);
+    if (VK_SUCCESS != result) {
+        LOG_ERROR("[InstanceLayerProperties] Failed to enumerate instance layer property count.");
+        goto cleanup_pager;
     }
 
-    vkc_instance_destroy(instance);
-    LOG_INFO("Successfully destroyed Vulkan instance!");
+    VkLayerProperties* vkInstanceLayerProperties = page_malloc(
+        pager,
+        vkInstanceLayerPropertyCount * sizeof(VkLayerProperties),
+        alignof(VkLayerProperties)
+    );
+    if (NULL == vkInstanceLayerProperties) {
+        LOG_ERROR(
+            "[InstanceLayerProperties] Failed to allocate %u instance layer property objects.", 
+            vkInstanceLayerPropertyCount
+        );
+        goto cleanup_pager;
+    }
+    memset(vkInstanceLayerProperties, 0, vkInstanceLayerPropertyCount * sizeof(VkLayerProperties));
+
+    result = vkEnumerateInstanceLayerProperties(
+        &vkInstanceLayerPropertyCount, vkInstanceLayerProperties
+    );
+    if (VK_SUCCESS != result) {
+        LOG_ERROR("[InstanceLayerProperties] Failed to enumerate instance layer properties.");
+        goto cleanup_pager;
+    }
+
+#if defined(VKC_DEBUG) && (1 == VKC_DEBUG)
+    // Log the results to standard output
+    LOG_DEBUG("[InstanceLayerProperties] Found %u instance layer properties.", vkInstanceLayerPropertyCount);
+    for (uint32_t i = 0; i < vkInstanceLayerPropertyCount; i++) {
+        LOG_DEBUG("[InstanceLayerProperties] i=%u, name=%s, description=%s", 
+            i, vkInstanceLayerProperties[i].layerName, vkInstanceLayerProperties[i].description
+        );
+    }
+#endif
+
+    bool vkInstanceLayerPropertyFound = false;
+    char const* vkInstanceLayerPropertyNames[] = {"VK_LAYER_KHRONOS_validation"};
+    for (uint32_t i = 0; i < vkInstanceLayerPropertyCount; i++) {
+        if (0 == utf8_raw_compare(vkInstanceLayerProperties[i].layerName, vkInstanceLayerPropertyNames[0])) {
+            vkInstanceLayerPropertyFound = true;
+            LOG_INFO("[InstanceCreateInfo] Enabling Layer: %s", vkInstanceLayerProperties[i].layerName);
+            break;
+        }
+    }
+
+    /** @} */
+
+    /**
+     * @name Instance Extension Properties
+     * @{
+     */
+
+    uint32_t vkInstanceExtensionPropertyCount = 0;
+    result = vkEnumerateInstanceExtensionProperties(NULL, &vkInstanceExtensionPropertyCount, NULL);
+    if (VK_SUCCESS != result) {
+        LOG_ERROR("[InstanceExtensionProperties] Failed to enumerate instance extension property count.");
+        goto cleanup_pager;
+    }
+
+    VkExtensionProperties* vkInstanceExtensionProperties = page_malloc(
+        pager,
+        vkInstanceExtensionPropertyCount * sizeof(VkExtensionProperties),
+        alignof(VkExtensionProperties)
+    );
+    if (NULL == vkInstanceExtensionProperties) {
+        LOG_ERROR("[InstanceExtensionProperties] Failed to allocate %u instance extension property objects.", vkInstanceExtensionPropertyCount);
+        goto cleanup_pager;
+    }
+    memset(vkInstanceExtensionProperties, 0, vkInstanceExtensionPropertyCount * sizeof(VkExtensionProperties));
+
+    result = vkEnumerateInstanceExtensionProperties(NULL, &vkInstanceExtensionPropertyCount, vkInstanceExtensionProperties);
+    if (VK_SUCCESS != result) {
+        LOG_ERROR("[InstanceExtensionProperties] Failed to enumerate instance extension properties.");
+        goto cleanup_pager;
+    }
+
+#if defined(VKC_DEBUG) && (1 == VKC_DEBUG)
+    // Log the results to standard output
+    LOG_DEBUG("[InstanceExtensionProperties] Found %u instance extension properties.", vkInstanceExtensionPropertyCount);
+    for (uint32_t i = 0; i < vkInstanceExtensionPropertyCount; i++) {
+        LOG_DEBUG("[InstanceExtensionProperties] i=%u, name=%s, version=%u", 
+            i, vkInstanceExtensionProperties[i].extensionName, vkInstanceExtensionProperties[i].specVersion
+        );
+    }
+#endif
+
+    uint32_t vkInstanceExtensionPropertyNameCount = 6;
+    char const* vkInstanceExtensionPropertyNames[] = {
+        "VK_KHR_device_group_creation",
+        "VK_KHR_external_fence_capabilities",
+        "VK_KHR_external_memory_capabilities",
+        "VK_KHR_external_semaphore_capabilities",
+        "VK_KHR_get_physical_device_properties2",
+        "VK_EXT_debug_utils",
+    };
+
+    bool vkInstanceExtensionPropertyFound = true;
+    for (uint32_t i = 0; i < vkInstanceExtensionPropertyNameCount; i++) {
+        bool found = false;
+        for (uint32_t j = 0; j < vkInstanceExtensionPropertyCount; j++) {
+            if (0 == utf8_raw_compare(
+                vkInstanceExtensionPropertyNames[i],
+                vkInstanceExtensionProperties[j].extensionName)) {
+                found = true;
+                LOG_INFO("[InstanceCreateInfo] Enabling Extension: %s", vkInstanceExtensionPropertyNames[i]);
+                break;
+            }
+        }
+
+        if (!found) {
+            LOG_WARN("[InstanceCreateInfo] Extension not available: %s", vkInstanceExtensionPropertyNames[i]);
+            vkInstanceExtensionPropertyFound = false;
+        }
+    }
+
+    /** @} */
+
+    /**
+     * @name Vulkan Instance
+     * @{
+     */
+
+    uint32_t vkInstanceAPIVersion;
+    if (VK_SUCCESS != vkEnumerateInstanceVersion(&vkInstanceAPIVersion)) {
+        LOG_ERROR("Failed to enumerate instance API version.");
+        return 0;
+    }
+    
+    VkApplicationInfo vkInstanceAppInfo = {
+        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pApplicationName = "compute",
+        .applicationVersion = vkInstanceAPIVersion,
+        .pEngineName = "compute engine",
+        .engineVersion = vkInstanceAPIVersion,
+        .apiVersion = vkInstanceAPIVersion,
+    };
+
+    LOG_INFO("[VkApplicationInfo] Name: %s", vkInstanceAppInfo.pApplicationName);
+    LOG_INFO(
+        "[VkApplicationInfo] Version: %u.%u.%u",
+        VK_VERSION_MAJOR(vkInstanceAppInfo.applicationVersion),
+        VK_VERSION_MINOR(vkInstanceAppInfo.applicationVersion),
+        VK_VERSION_PATCH(vkInstanceAppInfo.applicationVersion)
+    );
+    LOG_INFO("[VkApplicationInfo] Engine Name: %s", vkInstanceAppInfo.pEngineName);
+    LOG_INFO(
+        "[VkApplicationInfo] Engine Version: %u.%u.%u",
+        VK_VERSION_MAJOR(vkInstanceAppInfo.engineVersion),
+        VK_VERSION_MINOR(vkInstanceAppInfo.engineVersion),
+        VK_VERSION_PATCH(vkInstanceAppInfo.engineVersion)
+    );
+    LOG_INFO(
+        "[VkApplicationInfo] API Version: %u.%u.%u",
+        VK_API_VERSION_MAJOR(vkInstanceAppInfo.apiVersion),
+        VK_API_VERSION_MINOR(vkInstanceAppInfo.apiVersion),
+        VK_API_VERSION_PATCH(vkInstanceAppInfo.apiVersion)
+    );
+
+    VkInstanceCreateInfo vkInstanceCreateInfo = {
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pApplicationInfo = &vkInstanceAppInfo,
+        .enabledExtensionCount = vkInstanceExtensionPropertyCount,
+        .ppEnabledExtensionNames = vkInstanceExtensionPropertyNames,
+    };
+
+    if (vkInstanceLayerPropertyFound) {
+        vkInstanceCreateInfo.enabledLayerCount = 1;
+        vkInstanceCreateInfo.ppEnabledLayerNames = vkInstanceLayerPropertyNames;
+    }
+
+    if (vkInstanceExtensionPropertyFound) {
+        vkInstanceCreateInfo.enabledExtensionCount = vkInstanceExtensionPropertyNameCount;
+        vkInstanceCreateInfo.ppEnabledExtensionNames = vkInstanceExtensionPropertyNames;
+    }
+
+    VkInstance vkInstance = VK_NULL_HANDLE;
+    result = vkCreateInstance(&vkInstanceCreateInfo, &vkAllocationCallback, &vkInstance);
+    if (VK_SUCCESS != result) {
+        LOG_ERROR("[VkInstance] Failed to create instance object: %d", result);
+        goto cleanup_pager;
+    }
+
+    // Free dead weight
+    page_free(pager, vkInstanceLayerProperties);
+    page_free(pager, vkInstanceExtensionProperties);
+
+    LOG_INFO("[VkInstance] Created instance @ %p.", vkInstance);
+
+    /** @} */
+
+
+    /**
+     * @name Clean up on Success
+     * @{
+     */
+
+    vkDestroyInstance(vkInstance, &vkAllocationCallback);
+    page_allocator_free(pager);
+
+#if defined(VKC_DEBUG) && (1 == VKC_DEBUG)
+    LOG_DEBUG("[VkCompute] Debug Mode: Exit Success");
+#else
+    LOG_INFO("[VkCompute] Release Mode: Exit Success");
+#endif
+
     return EXIT_SUCCESS;
+
+    /** {@ */
+
+    /**
+     * @name Clean up on Failure
+     * @{
+     */
+
+    vkDestroyInstance(vkInstance, &vkAllocationCallback);
+cleanup_pager:
+    page_allocator_free(pager);
+
+#if defined(VKC_DEBUG) && (1 == VKC_DEBUG)
+    LOG_DEBUG("[VkCompute] Debug Mode: Exit Failure");
+#else
+    LOG_INFO("[VkCompute] Release Mode: Exit Failure");
+#endif
+
+    return EXIT_FAILURE;
+
+    /** @} */
 }
