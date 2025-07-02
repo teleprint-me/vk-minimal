48bed7f7c099257245298ae036e206ea27662cd1
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-07-01 20:31:40 -0400

    instance(feat): Add abstraction for handling instance layer properties

--- src/vk/instance.c
+++ src/vk/instance.c
@@ -12,148 +12,70 @@
 #include "vk/instance.h"
 
 /**
- * @section Private Methods
- * {@
+ * @name VkC Instance Layer
+ * @{
  */
 
- /**
-  * @brief Enumerates the instance version.
-  */
-static uint32_t vkc_instance_init_version(void) {
-    uint32_t apiVersion;
-    if (VK_SUCCESS != vkEnumerateInstanceVersion(&apiVersion)) {
-        LOG_ERROR("Failed to enumerate Vulkan instance version.");
-        return 0;
+typedef struct VkcInstanceLayer {
+    PageAllocator* pager;
+    VkLayerProperties* properties;
+    uint32_t count;
+} VkcInstanceLayer;
+
+VkcInstanceLayer* vkc_instance_layer_create(size_t initial_size) {
+    PageAllocator* pager = page_allocator_create(initial_size);
+    if (NULL == pager) {
+        LOG_ERROR("[VkcInstanceLayer] Failed to create allocator (initial_size=%zu).", initial_size);
+        return NULL;
     }
-    return apiVersion;
+
+    VkcInstanceLayer* layer = page_malloc(pager, sizeof(VkcInstanceLayer), alignof(VkcInstanceLayer));
+    if (NULL == layer) {
+        return NULL;
+    }
+
+    layer->pager = pager;
+    layer->properties = NULL;
+    layer->count = 0;
+
+    VkResult result;
+    result = vkEnumerateInstanceLayerProperties(&layer->count, NULL);
+    if (VK_SUCCESS != result) {
+        LOG_ERROR("[VkcInstanceLayer] Failed to enumerate instance layer property count.");
+        return NULL;
+    }
+
+    layer->properties = page_malloc(
+        pager, layer->count * sizeof(VkLayerProperties), alignof(VkLayerProperties));
+    if (NULL == layer->properties) {
+        LOG_ERROR("[VkcInstanceLayer] Failed to allocate %u instance layer properties.", layer->count);
+        return NULL;
+    }
+    memset(layer->properties, 0, layer->count * sizeof(VkLayerProperties));
+
+    result = vkEnumerateInstanceLayerProperties(&layer->count, layer->properties);
+    if (VK_SUCCESS != result) {
+        LOG_ERROR("[VkcInstanceLayer] Failed to enumerate instance layer properties.");
+        return NULL;
+    }
+
+    layer->pager = pager;
+    return layer;
 }
 
-/**
- * @brief Initializes the Vulkan application info.
- */
-static void
-vkc_instance_init_app_info(VkcInstance* instance, const char* name, const char* engine) {
-    instance->version = vkc_instance_init_version();
-
-    instance->app_info = (VkApplicationInfo) {
-        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
-        .pApplicationName = name,
-        .applicationVersion = instance->version,
-        .pEngineName = engine,
-        .engineVersion = instance->version,
-        .apiVersion = instance->version,
-    };
-
-#if defined(DEBUG) && (1 == DEBUG)
-    LOG_DEBUG("Application Name: %s", instance->app_info.pApplicationName);
-    LOG_DEBUG(
-        "Application Version: %u.%u.%u",
-        VK_VERSION_MAJOR(instance->app_info.applicationVersion),
-        VK_VERSION_MINOR(instance->app_info.applicationVersion),
-        VK_VERSION_PATCH(instance->app_info.applicationVersion)
-    );
-    LOG_DEBUG("Engine Name: %s", instance->app_info.pEngineName);
-    LOG_DEBUG(
-        "Engine Version: %u.%u.%u",
-        VK_VERSION_MAJOR(instance->app_info.engineVersion),
-        VK_VERSION_MINOR(instance->app_info.engineVersion),
-        VK_VERSION_PATCH(instance->app_info.engineVersion)
-    );
-    LOG_DEBUG(
-        "API Version: %u.%u.%u",
-        VK_API_VERSION_MAJOR(instance->app_info.apiVersion),
-        VK_API_VERSION_MINOR(instance->app_info.apiVersion),
-        VK_API_VERSION_PATCH(instance->app_info.apiVersion)
-    );
-#endif
-}
-
-/**
- * @brief Initializes the VkInstanceCreateInfo structure.
- */
-static void vkc_instance_init_info(VkcInstance* instance) {
-    instance->create_info = (VkInstanceCreateInfo) {
-        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-        .pApplicationInfo = &instance->app_info,
-    };
-
-#if defined(DEBUG) && (1 == DEBUG)
-    VkcValidationLayer* validation
-        = vkc_validation_layer_create((const char*[]) {"VK_LAYER_KHRONOS_validation"}, 1, 1024);
-
-    if (validation && vkc_validation_layer_match_request(validation)) {
-        instance->create_info.enabledLayerCount = validation->request->count;
-        instance->create_info.ppEnabledLayerNames = validation->request->names;
-        LOG_DEBUG("Validation layers enabled.");
-    }
-
-    VkcExtension* extension = vkc_extension_create((const char*[]) {"VK_EXT_debug_utils"}, 1, 1024);
-
-    if (extension && vkc_extension_match_request(extension)) {
-        instance->create_info.enabledExtensionCount = extension->request->count;
-        instance->create_info.ppEnabledExtensionNames = extension->request->names;
-        LOG_DEBUG("Instance extensions enabled.");
-    }
-
-    vkc_validation_layer_free(validation);
-    vkc_extension_free(extension);
-#endif
+void vkc_instance_layer_free(VkcInstanceLayer* layer) {
+    if (NULL == layer || NULL == layer->pager) return;
+    PageAllocator* pager = layer->pager;
+    page_free(pager, layer->properties);
+    page_free(pager, layer);
+    page_allocator_free(pager);
 }
 
 /** @} */
 
 /**
- * @section Public Methods
- * {@
+ * @name VkC Instance Layer Names
+ * @{
  */
-
-/**
- * @brief Creates a Vulkan instance with internal allocator and metadata.
- *
- * @param page_size Initial size of the PageAllocator.
- * @return Pointer to the created instance, or NULL on failure.
- */
-VkcInstance* vkc_instance_create(size_t page_size) {
-    PageAllocator* pager = page_allocator_create(page_size);
-    if (NULL == pager) {
-        LOG_ERROR("Failed to create PageAllocator.");
-        return NULL;
-    }
-
-    VkcInstance* instance = page_malloc(pager, sizeof(VkcInstance), alignof(VkcInstance));
-    if (NULL == instance) {
-        LOG_ERROR("Failed to allocate VkcInstance.");
-        page_allocator_free(pager);
-        return NULL;
-    }
-
-    instance->pager = pager;
-    instance->allocator = vkc_page_callbacks(instance->pager);
-
-    vkc_instance_init_app_info(instance, "Compute", "Compute Engine");
-    vkc_instance_init_info(instance);
-
-    VkResult result = vkCreateInstance(&instance->create_info, &instance->allocator, &instance->object);
-    if (VK_SUCCESS != result) {
-        LOG_ERROR("vkCreateInstance failed: %d", result);
-        page_allocator_free(pager);
-        return NULL;
-    }
-
-#if defined(DEBUG) && (1 == DEBUG)
-    LOG_DEBUG("Vulkan instance created successfully @ %p", (void*) instance);
-#endif
-
-    return instance;
-}
-
-void vkc_instance_destroy(VkcInstance* instance) {
-    if (instance && instance->pager) {
-        if (VK_NULL_HANDLE != instance->object) {
-            vkDestroyInstance(instance->object, &instance->allocator);
-        }
-        page_allocator_free(instance->pager);
-    }
-}
 
 /** @} */
