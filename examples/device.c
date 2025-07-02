eada94f60f7a0dc871d1263041324e1eef959a22
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-07-02 14:59:12 -0400

    device(refactor): Encapsulate physical device enumeration

--- examples/device.c
+++ examples/device.c
@@ -8,6 +8,76 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct VkcPhysicalDeviceList {
+    PageAllocator* pager;
+    VkPhysicalDevice* devices;
+    uint32_t count;
+} VkcPhysicalDeviceList;
+
+VkcPhysicalDeviceList* vkc_physical_device_list_create(VkcInstance* instance) {
+    PageAllocator* pager = page_allocator_create(8);
+    if (!pager) {
+        return NULL;
+    }
+
+    VkcPhysicalDeviceList* list = page_malloc(pager, sizeof(*list), alignof(*list));
+    if (!list) {
+        return NULL;
+    }
+
+    *list = (VkcPhysicalDeviceList) {
+        .pager = pager,
+        .devices = NULL,
+        .count = 0,
+    };
+
+    VkResult result = vkEnumeratePhysicalDevices(instance->object, &list->count, NULL);
+    if (VK_SUCCESS != result || 0 == list->count) {
+        LOG_ERROR(
+            "[VkPhysicalDevice] No Vulkan-compatible devices found (VkResult: %d, Count: %u)",
+            result,
+            list->count
+        );
+        return NULL;
+    }
+
+    list->devices = page_malloc(
+        pager,
+        list->count * sizeof(VkPhysicalDevice),
+        alignof(VkPhysicalDevice)
+    );
+    if (NULL == list->devices) {
+        LOG_ERROR("[VkPhysicalDevice] Failed to allocate device list.");
+        return NULL;
+    }
+
+    result = vkEnumeratePhysicalDevices(instance->object, &list->count, list->devices);
+    if (VK_SUCCESS != result) {
+        LOG_ERROR("[VkPhysicalDevice] Failed to enumerate devices (VkResult: %d)", result);
+        return NULL;
+    }
+
+#if defined(VKC_DEBUG) && (1 == VKC_DEBUG)
+    LOG_DEBUG("[VkcPhysicalDeviceList] Found %u devices.", list->count);
+    for (uint32_t i = 0; i < list->count; i++) {
+        VkPhysicalDevice device = list->devices[i];
+        VkPhysicalDeviceProperties properties = {0};
+        vkGetPhysicalDeviceProperties(device, &properties);    
+        LOG_DEBUG("[VkcPhysicalDeviceList] i=%u, name=%s, type=%d", 
+            i, properties.deviceName, (int) properties.deviceType
+        );
+    }
+#endif
+
+    return list;
+}
+
+void vkc_physical_device_list_free(VkcPhysicalDeviceList* list) {
+    if (list && list->pager) {
+        page_allocator_free(list->pager);
+    }
+}
+
 int main(void) {
     /**
      * @name Debug Environment
@@ -67,22 +137,32 @@ int main(void) {
 
     VkcInstance* instance = vkc_instance_create(layer_match, extension_match);
     if (!instance) {
-        goto cleanup_instance;
+        goto cleanup_instance_layer;
     }
 
     /** @} */
 
+    VkcPhysicalDeviceList* device_list = vkc_physical_device_list_create(instance);
+    if (!device_list) {
+        goto cleanup_instance;
+    }
 
     /**
      * @name Clean up on Success
      * @{
      */
 
+    // Device List
+    vkc_physical_device_list_free(device_list);
+
+    // Instance
+    vkc_instance_free(instance);
+    // Instance Extensions
     vkc_instance_extension_match_free(extension_match);
     vkc_instance_extension_free(extension);
+    // Instance Layers
     vkc_instance_layer_match_free(layer_match);
     vkc_instance_layer_free(layer);
-    vkc_instance_free(instance);
 
 #if defined(VKC_DEBUG) && (1 == VKC_DEBUG)
     LOG_DEBUG("[VkCompute] Debug Mode: Exit Success");
@@ -99,7 +179,10 @@ int main(void) {
      * @{
      */
 
-cleanup_instance:
+cleanup_instance: 
+    vkc_instance_free(instance);
+
+cleanup_instance_layer:
     vkc_instance_extension_match_free(extension_match);
     vkc_instance_extension_free(extension);
     vkc_instance_layer_match_free(layer_match);
